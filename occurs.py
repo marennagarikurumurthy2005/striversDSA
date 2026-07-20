1afba83bb1962cc7e36fbfcb6511367d5ff0cd1c
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-20 22:43:09 +0530

    method 2

--- occurs.py
+++ occurs.py
@@ -153,7 +153,32 @@ else:
     
 
 
+# m=len(nums1)
+#         n=len(nums2)
+#         total=m+n
+#         i=j=0
+#         prev=0
+#         curr=0
+#         for i in range((total//2)+1):
+#             prev=curr
+#             if (i<m and j<n):
+#                 if nums1[i]<=nums2[j]:
+#                     curr=nums1[i]
+#                     i+=1
+#                 else:
+#                     curr=nums2[j]
+#                     j+=1
+#             elif i<m:
+#                 curr=nums1[i]
+#                 i+=1
 
+#             else:
+#                 curr=nums2[j]
+#                 j+=1
+#         if total%2==1:
+#             return curr
+#         else:
+#             return (prev+curr)/2.0
 
 
 
