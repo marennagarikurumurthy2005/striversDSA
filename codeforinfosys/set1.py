a8a0cbe062a44ddc43c0e305d0fb5fa92ab5e614
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-23 23:08:04 +0530

    max elements in window

--- codeforinfosys/set1.py
+++ codeforinfosys/set1.py
@@ -29,24 +29,67 @@
 # A subarray is a contiguous non-empty sequence of elements within an array.
 # Example 1:
 
-nums = [1,1,1]
-k = 2
-# Output: 2
-# Example 2:
+# nums = [1,1,1]
+# k = 2
+# # Output: 2
+# # Example 2:
 
-# Input: nums = [1,2,3], k = 3
-# Output: 2
+# # Input: nums = [1,2,3], k = 3
+# # Output: 2
 
-count=0
-prev_sum=0
-d={0:1}
-for i in nums:
-    prev_sum+=i
-    if prev_sum-k in d:
-        count+=d[prev_sum-k]
-    if prev_sum in d:
-        d[i]+=1
-    else:
-        d[i]=1
-print(count)
+# count=0
+# prev_sum=0
+# d={0:1}
+# for i in nums:
+#     prev_sum+=i
+#     if prev_sum-k in d:
+#         count+=d[prev_sum-k]
+#     if prev_sum in d:
+#         d[i]+=1
+#     else:
+#         d[i]=1
+# print(count)
+
+
+
+# 239. Sliding Window Maximum
+# Solved
+# Hard
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+# Return the max sliding window.
+# Example 1:
+
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+
+from collections import deque
+nums =[1,3,-1,-3,5,3,6,7]
+k =3
+ans=[]
+dq=deque()
+for i in range(len(nums)):
+    while dq and dq[0]<=i-k:
+        dq.popleft()
+    while dq and nums[dq[-1]]<nums[i]:
+        dq.pop()
+    dq.append(i)
+    if i>=k-1:
+        ans.append(nums[dq[0]])
+print(ans)
+
+
+    
 
